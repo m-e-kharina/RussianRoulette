68632b6d107b25d540a8ef9bfae7f85121b7eff7
Marina Kharina <marina_kharina@epam.com>
2023-11-09 14:56:21 +0300

    Init commit

---
+++ src/Cell.java
@@ -0,0 +1,15 @@
+public class Cell {
+
+    /**
+     isLucky - Показатель "удачной" (пустой) ячейки. По умолчанию делаем ячейку пустой.
+     */
+    private boolean isLucky = true;
+
+    public boolean getIsLucky() {
+        return isLucky;
+    }
+
+    public void setIsLucky(boolean isLucky) {
+        this.isLucky = isLucky;
+    }
+}
